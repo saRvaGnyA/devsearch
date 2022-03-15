dd63184e8ef3a93e136eedef3e41fcf2a4c365f6
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-03-15 19:41:36 +0530

    rendering data in jinjas

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,10 +3,36 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = "Welcome to the Projects page!"
+    number = 9
+    context = {"message": msg, "no": number, "projects": projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    obj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            obj = i
+    context = {'project': obj}
+    return render(request, 'projects/single-project.html', context)
